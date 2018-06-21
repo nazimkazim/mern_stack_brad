1a1ca168d0dfd8f7c82662d6897833735f51ed65
Your Name <nazik_111@mail.ru>
2018-06-21 18:08:16 +0500

    add get post action and post feed component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-import { ADD_POST, GET_ERRORS } from './types';
+import {
+  ADD_POST,
+  GET_ERRORS,
+  GET_POSTS,
+  GET_POST,
+  POST_LOADING
+} from './types';
 
 // Add post
 export const addPost = postData => dispatch => {
@@ -8,4 +14,30 @@ export const addPost = postData => dispatch => {
     .post('/api/posts', postData)
     .then(res => dispatch({ type: ADD_POST, payload: res.data }))
     .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
+// GET posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get('/api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Set loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -8,5 +8,6 @@ export const GET_PROFILES = 'GET_PROFILES';
 export const POST_LOADING = 'POST_LOADING';
 export const ADD_POST = 'ADD_POST';
 export const GET_POST = 'GET_POST';
+export const GET_POSTS = 'GET_POSTS';
 export const GET_ADD = 'GET_ADD';
 export const DELETE_PROFILES = 'DELETE_PROFILES';
